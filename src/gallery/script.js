aa140feb96fd8ed1a6d5091cac14a93156cd7204
zolcsika71 <bushdoctor2008@gmail.com>
2020-05-11 01:06:13 +0200

    NEW PHOTOS

--- src/gallery/script.js
+++ src/gallery/script.js
@@ -4,38 +4,38 @@ let photoArray = [],
     currentPhoto = 0,
     data1 = {
     photo:  './img/1.jpg',
-    title: '1st title',
-    description: '1st description'
+    title: 'Last Year',
+    description: 'It was before'
 },
     data2 = {
         photo:  './img/2.jpg',
-        title: '2nd title',
-        description: '2nd description'
+        title: 'This Year',
+        description: 'It`s scary'
     },
     data3 = {
         photo:  './img/3.jpg',
-        title: '3rd title',
-        description: '3rd description'
+        title: 'Science',
+        description: 'It`s not that science'
     },
     data4 = {
         photo:  './img/4.jpg',
-        title: '4th title',
-        description: '4th description'
+        title: 'Future',
+        description: 'Will be told'
     },
     data5 = {
         photo:  './img/5.jpg',
-        title: '5th title',
-        description: '5th description'
+        title: 'Shout',
+        description: 'We must to say our words'
     },
     data6 = {
         photo:  './img/6.jpg',
-        title: '6th title',
-        description: '6th description'
+        title: 'Play',
+        description: 'Or play it gently'
     },
     data7 = {
         photo:  './img/7.jpg',
-        title: '7th title',
-        description: '7th description'
+        title: `Holnap`,
+        description: 'Tomorrow is Another Day'
     };
 
 photoArray.push(data1, data2, data3, data4, data5, data6, data7);
